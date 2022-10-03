c9f63150ba5607e46a71d6a95c6951f0a93119d2
shinya-ml <shinya_suzuki@voyagegroup.com>
2022-10-03 15:14:39 +0900

    example

--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,5 @@
 module backend_example
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- example/main.go
+++ example/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/ping", func(_ http.ResponseWriter, _ *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", func(_ http.ResponseWriter, _ *http.Request) {
 		fmt.Println("pong")
 	})
 
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":80",
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("error: %v", err)
+	}
 }
